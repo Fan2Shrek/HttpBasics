29d520927ed24b3e82a29065245e8a83341611c5
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-04-08 10:39:26 +0200

    fix docs

--- Response/JsonResponse.php
+++ Response/JsonResponse.php
@@ -6,6 +6,7 @@ class JsonResponse extends AbstractResponse
 {
     public function __invoke()
     {
+        header('Content-Type: application/json');
         $this->setContent(json_encode($this->getContent()));
         parent::__invoke();
     }
